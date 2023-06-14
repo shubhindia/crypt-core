b23fff685fb4d6b07fa78a7beb75f12a39947a9a
Shubham Gopale <sgopale@cisco.com>
2023-06-14 21:41:18 +0530

    add k8s provider 🍱
    Signed-off-by: Shubham Gopale <sgopale@cisco.com>

--- providers/encryptDecrypt.go
+++ providers/encryptDecrypt.go
@@ -1,8 +1,13 @@
 package providers
 
 import (
+	"context"
 	"fmt"
 	"os"
+
+	"github.com/shubhindia/crypt-core/providers/utils"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func DecodeAndDecrypt(encoded string, provider string) (string, error) {
@@ -15,6 +20,31 @@ func DecodeAndDecrypt(encoded string, provider string) (string, error) {
 			return "", fmt.Errorf("keyphrase not found")
 		}
 
+		decoded, err := staticDecodeAndDecrypt(encoded, keyPhrase)
+		if err != nil {
+			return "", err
+		}
+		return decoded, nil
+
+	case "k8s":
+		k8sClient, err := utils.GetKubeClient()
+		if err != nil {
+			return "", fmt.Errorf("failed to get kubeclient %v", err)
+		}
+		// Define the namespace and secret name to retrieve
+		namespace := "default"
+		secretName := "default"
+
+		// Retrieve the secret from the Kubernetes cluster
+		secret, err := k8sClient.CoreV1().Secrets(namespace).Get(context.TODO(), secretName, metav1.GetOptions{})
+		if err != nil {
+			return "", fmt.Errorf("failed to get the secret %v", err)
+		}
+
+		// Access the secret data"
+		keyPhrase := string(secret.Data["token"])
+
+		// decode the data
 		decoded, err := staticDecodeAndDecrypt(encoded, keyPhrase)
 		if err != nil {
 			return "", err
@@ -35,6 +65,27 @@ func EncryptAndEncode(value string, provider string) (string, error) {
 		if keyPhrase == "" {
 			return "", fmt.Errorf("keyphrase not found")
 		}
+		return staticEncryptAndEncode(value, keyPhrase)
+
+	case "k8s":
+		k8sClient, err := utils.GetKubeClient()
+		if err != nil {
+			return "", fmt.Errorf("failed to get kubeclient %v", err)
+		}
+		// Define the namespace and secret name to retrieve
+		namespace := "default"
+		secretName := "default"
+
+		// Retrieve the secret from the Kubernetes cluster
+		secret, err := k8sClient.CoreV1().Secrets(namespace).Get(context.TODO(), secretName, metav1.GetOptions{})
+		if err != nil {
+			return "", fmt.Errorf("failed to get the secret %v", err)
+		}
+
+		// Access the secret data"
+		keyPhrase := string(secret.Data["token"])
+
+		// encrypt the data
 		return staticEncryptAndEncode(value, keyPhrase)
 
 	}
--- providers/utils/utils.go
+++ providers/utils/utils.go
@@ -3,10 +3,69 @@ package utils
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 func MdHashing(input string) string {
 	byteInput := []byte(input)
 	md5Hash := md5.Sum(byteInput)
 	return hex.EncodeToString(md5Hash[:]) // by referring to it as a string
+}
+
+func GetKubeClient() (*kubernetes.Clientset, error) {
+
+	// it running inside a k8s cluster, use InCLusterConfig
+	if runningInKubernetes() {
+		config, err := rest.InClusterConfig()
+		if err != nil {
+			return nil, fmt.Errorf("failed to build client configuration: %v", err)
+		}
+		// creates the clientset
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create Kubernetes client: %v", err)
+		}
+		return clientset, nil
+	}
+
+	var kubeconfig *string
+	home := homedir.HomeDir()
+	kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+
+	flag.Parse()
+
+	// Build the client configuration from the provided kubeconfig file
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build client configuration: %v", err)
+	}
+
+	// Create a new Kubernetes clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Kubernetes client: %v", err)
+	}
+	return clientset, err
+
+}
+
+func runningInKubernetes() bool {
+
+	// Check for the presence of common Kubernetes environment variables
+	envVars := []string{"KUBERNETES_SERVICE_HOST", "KUBERNETES_PORT"}
+	for _, envVar := range envVars {
+		if _, exists := os.LookupEnv(envVar); !exists {
+			return false
+		}
+	}
+
+	return true
 }
